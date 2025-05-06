acca10e0bd8fabb0f062cbd8bfaf4fbb211f299e
Monika <monika.cilinska@gmail.com>
2025-05-06 23:53:52 +0200

    installPrompt move to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-//
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import InstallPrompt from "../components/pwa/InstallPrompt";
 
 export const metadata: Metadata = {
   title: "Plants Doc",
@@ -43,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <InstallPrompt />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import InstallPrompt from "../components/pwa/InstallPrompt";
-
 export default function Home() {
   return (
    <>
-     <InstallPrompt />
-
      <div>stronka</div>
    </>
   );
